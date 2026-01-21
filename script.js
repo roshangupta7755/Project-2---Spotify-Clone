cbcba90a23b5f62fe62fecdef2493d63ec0bf642
Roshan Gupta <roshangupta112113@gmail.com>
2026-01-21 13:34:41 +0530

    Refactor music player functionality and UI interactions

--- script.js
+++ script.js
@@ -97,43 +97,101 @@ async function displayAlbums() {
 
 /* ================= MAIN ================= */
 async function main() {
-    // Load default album (ncs) on start
-    await getSongs("song/ncs");
-    playMusic(songs[0], true);
+	await getSongs("song/ncs");
+	playMusic(songs[0], true)
 
-    // Display album cards
-    await displayAlbums();
+	// Display all the albums on the page
+	await displayAlbums()
 
-    // Play/pause button logic
-    play.addEventListener("click", () => {
-        if (currentSong.paused) {
-            currentSong.play();
-            play.src = "img/pause.svg";
+	// Attach an event listener to play, next and previous
+	play.addEventListener("click", () => {
+		if (currentSong.paused) {
+			currentSong.play()
+			play.src = "img/pause.svg"
+		}
+		else {
+			currentSong.pause()
+			play.src = "img/play.svg"
+		}
+	})
+	// Listen for timeupdate event
+	currentSong.addEventListener("timeupdate", () => {
+		document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+		document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+	})
+	// Add an event listener to seekbar
+	document.querySelector(".seekbar").addEventListener("click", e => {
+		let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+		document.querySelector(".circle").style.left = percent + "%";
+		currentSong.currentTime = ((currentSong.duration) * percent) / 100
+	})
+	// Add event listener for hamburger
+	document.querySelector(".hamburger").addEventListener("click", () => {
+		document.querySelector(".left").style.left = "0"
+	})
+	// Add an event listener for close button
+	document.querySelector(".close").addEventListener("click", () => {
+		document.querySelector(".left").style.left = "-120%"
+	})
+
+	// Add an event listener to previous
+	previous.addEventListener("click", () => {
+		currentSong.pause()
+		console.log("Previous clicked")
+		let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+		if ((index - 1) >= 0) {
+			playMusic(songs[index - 1])
+		}
+	})
+
+	// Add an event listener to next
+	next.addEventListener("click", () => {
+		currentSong.pause()
+		console.log("Next clicked")
+
+		let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+		if ((index + 1) < songs.length) {
+			playMusic(songs[index + 1])
+		}
+	})
+
+	// Add event to volume
+	document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
+		console.log("Setting volume to", e.target.value, "/ 100")
+		currentSong.volume = parseInt(e.target.value) / 100
+		if (currentSong.volume > 0) {
+			document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
+		}
+	})
+
+	// Add an event listener to mute the track
+	document.querySelector(".volume>img").addEventListener("click", e => {
+		if (e.target.src.includes("volume.svg")) {
+			e.target.src = e.target.src.replace("volume.svg", "mute.svg")
+			currentSong.volume = 0;
+			document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
+		}
+		else {
+			e.target.src = e.target.src.replace("mute.svg", "volume.svg")
+			currentSong.volume = .10;
+			document.querySelector(".range").getElementsByTagName("input")[0].value = 10;
+		}
+
+	})
+
+	// Add an event click esc btn then page are refresh
+	document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        if (document.querySelector(".left").style.left === "0px") {
+            document.querySelector(".left").style.left = "-120%";
         } else {
-            currentSong.pause();
-            play.src = "img/play.svg";
+            location.reload();
         }
-    });
+    }
+});
 
-    // Update time display as song plays
-    currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML =
-            `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
-        document.querySelector(".circle").style.left =
-            (currentSong.currentTime / currentSong.duration) * 100 + "%";
-    });
-
-    // Seekbar click to jump in song
-    document.querySelector(".seekbar").addEventListener("click", e => {
-        let percent = (e.offsetX / e.target.clientWidth) * 100;
-        document.querySelector(".circle").style.left = percent + "%";
-        currentSong.currentTime = (currentSong.duration * percent) / 100;
-    });
-
-    // Press ESC to refresh page
-    document.addEventListener("keydown", e => {
-        if (e.key === "Escape") location.reload();
-    });
 }
 
 main();
+
+
